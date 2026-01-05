fb5a0e4f11d51f4dba40ce6e1eee14a95c9f379e
meanev56 <emekanjoku86@gmail.com>
2026-01-05 11:02:51 +0100

    chore: add pinia and pinia-plugin-persistedstate dependencies; create router index file

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate';
+import { router } from './router';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedState);
+
+const app = createApp(App);
+app.use(router);
+app.use(pinia);
+app.mount('#app');
